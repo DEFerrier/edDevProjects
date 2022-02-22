150ba0137e359a79ccc5c053d789fcf0264f5308
David Ferrier <david.e.ferrier02@gmail.com>
2022-02-21 20:54:40 -0500

    commit 1

--- BubbleSorter.java
+++ BubbleSorter.java
@@ -1,3 +1,39 @@
+package bubbleSorter;
+
+import java.util.*;
+
 public class BubbleSorter {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r = new Random ();
+		int[] nArray = new int[10];
+		for ( int i = 0 ; i < nArray.length ; i++ ){
+			nArray[ i ] = r.nextInt(30);}
+		int[] locations = new int[2];
+				for(int i = 0 ; i <locations.length ; i++) {locations [ i ] = i;}
+			
+	scanAndWriteArrays (nArray);
+	
+	print(nArray);
+	
+	
+		
+	
+	}
+
+
+
+
+	public static void scanAndWriteArrays (int [] nArray ) {	
+		
+		
+	}
+	
+	public static void print(int[] nArray) {
+		
+	}
+	
 }
+	
+	
