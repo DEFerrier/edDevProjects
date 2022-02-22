a712481fa098fad9a8d6e70c75cf453bac03ca13
David Ferrier <david.e.ferrier02@gmail.com>
2022-02-22 07:51:56 -0500

    This is the second commit
    It appears at this point in time that the bubble sorter works.  There are some fringe use cases that you'll want to pay attention to, like it won't work at all if the array is not of an appropriate size, and conceptually there is room to program those,  but since we define the array at the beginning,  the more sensible solution is to make sure you define your array as being more than 2 in length.

--- BubbleSorter.java
+++ BubbleSorter.java
@@ -1,5 +1,5 @@
 package bubbleSorter;
-
+//sorts highest right
 import java.util.*;
 
 public class BubbleSorter {
@@ -10,30 +10,84 @@ public class BubbleSorter {
 		int[] nArray = new int[10];
 		for ( int i = 0 ; i < nArray.length ; i++ ){
 			nArray[ i ] = r.nextInt(30);}
-		int[] locations = new int[2];
-				for(int i = 0 ; i <locations.length ; i++) {locations [ i ] = i;}
-			
-	scanAndWriteArrays (nArray);
-	
-	print(nArray);
-	
-	
 		
+	scanAndWriteArrays (nArray);	
 	
 	}
-
-
 
 
 	public static void scanAndWriteArrays (int [] nArray ) {	
 		
+		//this variable operator sets up the main while loop and allows us to write an exit condition
+		boolean aa = false;
 		
-	}
-	
-	public static void print(int[] nArray) {
+		int startVar1 = 0;
+		int endVar1 = 0;
 		
+		
+		//outlier use code
+		//if ( nArray.length < 1 ){
+		//	System.out.println("Add more numbers to sort!");
+		//}	
+		// outlier use code as well
+		//no longer works with redesign,  don't care to fix it since no reason
+		//if (nArray.length == 1) {continue;}
+		
+		//if ( nArray.length > 2 ) {	
+		//}
+		while (!aa) {
+			while (startVar1 < (nArray.length - 1)) {
+			int Holder1 = nArray[startVar1];
+			int Holder2 = nArray[startVar1 + 1]; 
+			int Comparator1 = Holder1;
+			int Comparator2 = Holder2;
+			
+			if ( Comparator1 < Comparator2 ) {
+			startVar1++;
+			endVar1++;
+				
+			}
+			
+			if ( Comparator1 == Comparator2 ) {
+				startVar1++;
+				endVar1++;
+			}
+			
+			if (Comparator1 > Comparator2) {
+				nArray[startVar1] = Holder2;
+				nArray[startVar1 + 1] = Holder1;
+				startVar1++;
+				endVar1 = 0;
+				
+				
+			}
+				
+			}
+			if (endVar1 >= nArray.length ) {
+				String output = "";
+				for (int i = 0 ; i < nArray.length ; i++) {
+					output += nArray[i] + ", ";
+					}
+				System.out.println(output);
+				aa = true ; 
+			} else {
+				startVar1 = 0;
+			}
 	}
 	
 }
+}
+
+
 	
 	
+ //-  
+ //declare variables 
+ 
+ //- -
+ //if a change is necessary write the values to arrays, decrease variable?
+ 
+ ///- - 
+// if a change is not necessary. increase the value of the initial variable, break?
+ 
+ 
